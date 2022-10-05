f62c54bb3870684ae3b099660519869ddd483ca4
홍혁준 <jjj8337721@naver.com>
2022-10-05 21:09:21 +0900

    config 연결

--- src/main/java/kr/co/configolineworkout/ConfigOlineWorkoutApplication.java
+++ src/main/java/kr/co/configolineworkout/ConfigOlineWorkoutApplication.java
@@ -2,8 +2,10 @@ package kr.co.configolineworkout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigOlineWorkoutApplication {
 
     public static void main(String[] args) {
