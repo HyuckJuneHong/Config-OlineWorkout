c58d7401fa4688e6d8c9ea738656b7ee7871e03d
홍혁준 <jjj8337721@naver.com>
2022-10-05 18:31:00 +0900

    upload an application yaml file

---
+++ src/main/java/kr/co/configolineworkout/ConfigOlineWorkoutApplication.java
@@ -0,0 +1,13 @@
+package kr.co.configolineworkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigOlineWorkoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigOlineWorkoutApplication.class, args);
+    }
+
+}
